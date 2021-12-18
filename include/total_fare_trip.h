772190bb9940ffc7f867ad15aad0a58dacac8a9d
imsarahkhan <92967389+imsarahkhan@users.noreply.github.com>
2021-12-18 12:00:03 +0300

    Create total_fare_trip.h

---
+++ include/total_fare_trip.h
@@ -0,0 +1,29 @@
+/**
+ * @file total_fare_trip.h
+ *
+ * @This provides the total fare for the entire trip if user wants to book flight, hotel and car services
+ *
+ *
+ * 
+ *
+ **/
+#ifndef _TOTAL_FARE_TRIP_H_
+#define _TOTAL_FARE_TRIP_H_
+
+/**
+* @brief This function will calaculate the total price for the  flight trip including the number
+*
+* of passengers travelling in flight for the specific routes
+*
+* 
+*
+* @param[in] The function takes input as number of passengers from user, price for
+*
+* each flight route and the booking ID created during booking trip
+*
+* @param[out] This function returns the total price of the flight trip
+*
+*
+**/
+extern float total_fare_trip (float total_flight, float total_hotel, float total_car);
+#endif
