e87fae0f64071311b19e39c79ed1ba4ee0a70484
kavyaram15 <35964082+kavyaram15@users.noreply.github.com>
2021-12-19 12:52:53 -0500

    Fixed few functions

--- src/global_variables.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by kavya on 18-12-2021.
-//
-#include "../include/global_variable.h"
-int g_booking_id=0,g_count_1=0,g_count_2=0,g_count_3=0,g_count_4=0,g_count_5=0,g_count_6=0,g_count_7=0,g_count_8=0,g_count_9=0,g_count_10=0;
